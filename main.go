82417d4f59539019056f504acee2389a0c430181
marc-4 <blaze.verdugo.s@gmail.com>
2025-10-30 15:14:40 +0800

    added go fiber

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"context"
+	"log"
 
 	"github.com/Marc-Moonshot/temporal-guru/db"
+	"github.com/gofiber/fiber/v3"
 )
 
 func main() {
 	conn := db.Connect()
 	defer conn.Close(context.Background())
+
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":8000"))
 }
