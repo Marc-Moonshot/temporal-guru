fff1c8ff95400dc47f84d48b223f5eadb3f94458
marc-4 <blaze.verdugo.s@gmail.com>
2025-10-30 14:16:35 +0800

    added db connection

--- db/connect.go
+++ db/connect.go
@@ -1,3 +1,32 @@
 package db
 
-// connects to postgre
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func Connect() *pgx.Conn {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: .env file not found. using system environment variables.")
+	}
+
+	dbUrl := os.Getenv("DB_URL")
+
+	if dbUrl == "" {
+		log.Fatal("DB_URL not set.")
+	}
+
+	conn, err := pgx.Connect(context.Background(), dbUrl)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+
+	fmt.Println("Connected to PostgreSQL.")
+	return conn
+}
--- main.go
+++ main.go
@@ -1,52 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"time"
+	"context"
+
+	"github.com/Marc-Moonshot/temporal-guru/db"
 )
 
 func main() {
-
-	if len(os.Args) < 2 {
-		fmt.Println("Pass at least 1 argument.")
-		return
-	}
-
-	var species = os.Args[1]
-
-	url := `https://pokeapi.co/api/v2/pokemon/` + species
-	fmt.Println(url)
-
-	httpClient := http.Client{
-		Timeout: time.Second * 5,
-	}
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	res, getErr := httpClient.Do(req)
-
-	if getErr != nil {
-		log.Fatal(getErr)
-	}
-
-	body, readErr := io.ReadAll(res.Body)
-
-	if readErr != nil {
-		log.Fatal(readErr)
-	}
-
-	var mon map[string]any
-	json.Unmarshal(body, &mon)
-
-	pretty, _ := json.MarshalIndent(mon, "", "  ")
-	fmt.Println(string(pretty))
+	conn := db.Connect()
+	defer conn.Close(context.Background())
 }
