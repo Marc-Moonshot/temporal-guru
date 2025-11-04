0a684e19727dd324ae1c79ab9bd60018828b57d6
marc-4 <blaze.verdugo.s@gmail.com>
2025-11-04 15:29:12 +0800

    added query parameter handling

--- api/routes.go
+++ api/routes.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 
@@ -15,22 +14,28 @@ import (
 func RegisterRoutes(app *fiber.App, conn *pgx.Conn) {
 
 	app.Get("/nrw/yearly", func(c fiber.Ctx) error {
-		fmt.Println("GET /nrw/yearly")
+		fmt.Println("[API] GET /nrw/yearly")
+
+		year := c.Queries()["year"]
+		device := c.Queries()["device"]
+
+		if year == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'year' parameter",
+			})
+		}
+
+		if device == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'device' parameter",
+			})
+		}
+
 		entry, err := cache.Get(conn, "nrw/yearly")
 
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
-				data, err := scheduler.Call("http://localhost/api/nrw/yearly", []string{"month=2025-11", "device=40961"}, conn)
-
-				if err != nil {
-					fmt.Printf("error: %v", err)
-				}
-
-				formatted, _ := json.MarshalIndent(data, "", "  ")
-				fmt.Printf("Data:\n%s\n", string(formatted))
-				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-					"error": "no cached data found",
-				})
+				return handleSchedulerCall(c, "http://localhost/api/nrw/yearly", []string{"year=" + year, "device=" + device}, conn)
 			}
 			fmt.Printf("error: %v\n", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -41,22 +46,28 @@ func RegisterRoutes(app *fiber.App, conn *pgx.Conn) {
 	})
 
 	app.Get("/nrw/monthly", func(c fiber.Ctx) error {
-		fmt.Println("GET /nrw/monthly")
+		fmt.Println("[API] GET /nrw/monthly")
+
+		month := c.Queries()["month"]
+		device := c.Queries()["device"]
+
+		if month == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'month' parameter",
+			})
+		}
+
+		if device == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'device' parameter",
+			})
+		}
+
 		entry, err := cache.Get(conn, "nrw/monthly")
 
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
-				data, err := scheduler.Call("http://localhost/api/nrw/monthly", []string{"month=2025-11", "device=40961"}, conn)
-
-				if err != nil {
-					fmt.Printf("error: %v", err)
-				}
-
-				formatted, _ := json.MarshalIndent(data, "", "  ")
-				fmt.Printf("Data:\n%s\n", string(formatted))
-				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-					"error": "no cached data found",
-				})
+				return handleSchedulerCall(c, "http://localhost/api/nrw/monthly", []string{"month=" + month, "device=" + device}, conn)
 			}
 			fmt.Printf("error: %v\n", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -67,22 +78,29 @@ func RegisterRoutes(app *fiber.App, conn *pgx.Conn) {
 	})
 
 	app.Get("/nrw/daily", func(c fiber.Ctx) error {
-		fmt.Println("GET /nrw/daily")
+
+		fmt.Println("[API] GET /nrw/daily")
+
+		month := c.Queries()["month"]
+		device := c.Queries()["device"]
+
+		if month == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'month' parameter",
+			})
+		}
+
+		if device == "" {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Missing 'device' parameter",
+			})
+		}
+
 		entry, err := cache.Get(conn, "nrw/daily")
 
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
-				data, err := scheduler.Call("http://localhost/api/nrw/daily", []string{"month=2025-11", "device=40961"}, conn)
-
-				if err != nil {
-					fmt.Printf("error: %v", err)
-				}
-
-				formatted, _ := json.MarshalIndent(data, "", "  ")
-				fmt.Printf("Data:\n%s\n", string(formatted))
-				return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-					"error": "no cached data found",
-				})
+				return handleSchedulerCall(c, "http://localhost/api/nrw/daily", []string{"month=" + month, "device=" + device}, conn)
 			}
 			fmt.Printf("error: %v\n", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -92,4 +110,25 @@ func RegisterRoutes(app *fiber.App, conn *pgx.Conn) {
 		return c.Status(fiber.StatusOK).JSON(entry)
 	})
 
+}
+
+func handleSchedulerCall(c fiber.Ctx, url string, params []string, conn *pgx.Conn) error {
+	fmt.Println("Invoking Scheduler.")
+	data, apiErr := scheduler.Call(url, params, conn)
+
+	if apiErr != nil {
+		fmt.Printf("error: %v", apiErr)
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Internal Server Error",
+		})
+	}
+
+	if data == nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "no cached data found",
+		})
+	}
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"payload": data,
+	})
 }
