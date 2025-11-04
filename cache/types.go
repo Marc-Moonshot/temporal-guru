fdbc8511857e55dfc35c632d4720f3b047e73452
marc-4 <blaze.verdugo.s@gmail.com>
2025-11-04 15:02:58 +0800

    updated types

--- cache/types.go
+++ cache/types.go
@@ -17,6 +17,7 @@ const (
 type CacheEntry struct {
 	Endpoint     string              `json:"endpoint"`
 	Query_params json.RawMessage     `json:"query_params"`
+	Params_hash  string 						 `json:"params_hash"`
 	Response     json.RawMessage     `json:"response"`
 	Fetched_at   timestamp.Timestamp `json:"fetched_at"`
 	Expires_at   timestamp.Timestamp `json:"expires_at"`
